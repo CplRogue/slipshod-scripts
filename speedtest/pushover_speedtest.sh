7bfb133644cbb25b6f0836d9bd7dbd03f1a06ab0
Pierus <darley+github@thewares.net>
2016-10-25 12:11:02 +0000

    Updated speedtest-cli.py

--- speedtest/pushover_speedtest.sh
+++ speedtest/pushover_speedtest.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-pushover_result=/home/mal/Scripts/speedtest/pushover_results
+result=$HOME/Scripts/speedtest/results.text
 pushover_app=/home/mal/Scripts/.dontsync/pushover_app_speedtest
 pushover_usr=/home/mal/Scripts/.dontsync/pushover_usr
 
-#/usr/bin/speedtest-cli --secure --simple > $pushover_result
-/usr/bin/speedtest-cli --simple > $pushover_result
+$HOME/Scripts/speedtest/speedtest_cli.py --secure --simple > $result
 
-if [[ -s $pushover_result ]] ; then
-echo "$pushover_result has data."
+if [[ -s $result ]] ; then
+echo $result
 #exit
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
-  --form-string "message=`cat $pushover_result`" \
+  --form-string "message=`cat $result`" \
   https://api.pushover.net/1/messages.json
 
 else
-echo "$pushover_result is empty."
+echo "$result is empty."
 fi ;
 
 exit
---
+++ speedtest/readme.txt
@@ -0,0 +1,5 @@
+Get the speedtest_cli.py program from:
+
+https://github.com/sivel/speedtest-cli/
+
+(https://github.com/sivel/speedtest-cli/archive/master.zip)
