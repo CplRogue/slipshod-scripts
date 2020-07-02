8bddedfa227ec96a2cd2c749a122b2017ed8a57b
CplRogue <darley+github@thewares.net>
2020-07-02 13:01:14 -0400

    Adding script to pushover change files

---
+++ filechanged/changedfile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+today=$(date +%Y%m%d-%H%M)
+file=$HOME/Scripts/TEST-CHANGEME
+temp=$HOME/Scripts/slipshod-scripts/filechanged/tmp
+backup=$HOME/Scripts/slipshod-scripts/filechanged/backup
+new=$temp/new.txt
+old=$temp/old.txt
+updated=$temp/diff.txt
+message=$temp/msg.txt
+pushover_app=$HOME/Scripts/.dontsync/pushover_app_changedfile
+pushover_usr=$HOME/Scripts/.dontsync/pushover_usr
+
+cp $new $old
+cp $file $new
+diff $new $old > $updated
+
+if [[ -s $updated ]] ; then
+echo "Bind has been updated" > $message
+cat $updated | tail -n +2 >> $message
+cat $message
+curl -s \
+  --form-string "token=`cat $pushover_app`" \
+  --form-string "user=`cat $pushover_usr`" \
+  --form-string "priority=0" \
+  --form-string "message=`cat $message`" \
+  https://api.pushover.net/1/messages.json > /dev/null 2>&1
+  cp $new $backup/$file_$today
+else
+  echo "$updated for Bind is zero."
+fi ;
+
