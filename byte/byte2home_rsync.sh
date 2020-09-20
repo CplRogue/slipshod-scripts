46f29a6c1ae9f89ed471606b87b135dfba0f7712
CplRogue <darley+github@thewares.net>
2020-09-20 15:47:35 -0400

    Partial Byte Sync rewrite

--- byte/byte2home_rsync.sh
+++ byte/byte2home_rsync.sh
@@ -3,13 +3,14 @@
 dst=`cat $HOME/Scripts/.dontsync/bysh_acct`
 remotehome=`cat $HOME/Scripts/.dontsync/bysh_home`
 
-local_temp_dir=/mnt/MrU/Working/zRsyncing/
+local_temp_dir=/mnt/MrU/Working/zRsyncing
 local_dir_tv=/mnt/MrU/Working/Sickrage/
 local_dir_movies=/mnt/MrU/Working/Movies/
 log=$HOME/Scripts/logs/log.byte2home
 remote_dir_tv=$remotehome/torrents/completed/
 remote_dir_movies=$remotehome/torrents/movies/
 
+running=`ps auxww | grep -v .git | grep /usr/bin/rsync`
 clear
 echo "####################################################################"
 echo "#                           Media Sync Script                      #"
@@ -18,22 +19,59 @@ echo " "
 echo " "
 echo " "
 echo " "
-echo "Pausing for 30 seconds to check that rsync is not already running"
-/usr/bin/killall rsync &>/dev/null
-sleep 5
-echo "            25 seconds"
-sleep 5
-echo "            20 seconds"
-sleep 5
-echo "            15 seconds"
-/usr/bin/killall rsync &>/dev/null
-sleep 5
-echo "            10 seconds"
-sleep 5
-echo "             5 seconds"
-sleep 5
-echo "             0 seconds"
-sleep 1 
+echo "Checking to see if RSYNC is already running"
+echo " "
+sleep 1
+
+if [[ "$running" == *"pierus"* ]]
+   then
+	   echo "RSYNC is runnning, stopping..."
+           /usr/bin/killall rsync &>/dev/null
+	   sleep 1
+	   echo "                   30 seconds"
+	   sleep 14
+           /usr/bin/killall rsync &>/dev/null
+	   echo "                   14 seconds"
+	   sleep 1
+	   echo "                   13 seconds"
+	   sleep 1
+	   echo "                   12 seconds"
+	   sleep 1
+	   echo "                   11 seconds"
+	   sleep 1
+	   echo "                   10 seconds"
+	   sleep 1
+	   echo "                   09 seconds"
+	   sleep 1
+	   echo "                   08 seconds"
+	   sleep 1
+	   echo "                   07 seconds"
+	   sleep 1
+	   echo "                   06 seconds"
+	   sleep 1
+	   echo "                   05 seconds"
+	   sleep 1
+	   echo "                   04 seconds"
+	   sleep 1
+	   echo "                   03 seconds"
+	   sleep 1
+	   echo "                   02 seconds"
+	   sleep 1
+	   echo "                   01 seconds"
+	   sleep 1
+           /usr/bin/killall rsync &>/dev/null
+   else	   
+	   echo "RSYNC not running, will continue in 5 seconds"
+	   sleep 1
+	   echo "                                    4 seconds"
+	   sleep 1
+	   echo "                                    3 seconds"
+	   sleep 1
+	   echo "                                    2 seconds"
+	   sleep 1
+	   echo "                                    1 second "
+	   sleep 1
+fi
 
 date >> $log
 
@@ -47,14 +85,17 @@ echo "Starting file transfer from Byte to Mr. Universe "
 date
 echo " "
 
-echo "TV Sync"
-echo " "
-echo "Files to transfer: "
+#echo "TV Sync"
+#echo " "
+echo "TV Files to transfer: "
 ssh $dst ls -R torrents/completed | grep -e .mkv -e .mp4
+echo " "
+echo "Movie Files to transfer: "
+ssh $dst ls -R torrents/movies | grep -e .mkv -e .mp4
 echo " "
 echo "TV Sync" >> $log
 #/usr/bin/rsync --remove-source-files --prune-empty-dirs --log-file=$log -rizcvIPe ssh  $dst:$remote_dir_tv $local_dir_tv 
-/usr/bin/rsync --remove-source-files --prune-empty-dirs --partial --temp-dir=$local_temp_dir --log-file=$log -rzvIPe ssh --stats $dst:$remote_dir_tv $local_dir_tv 
+/usr/bin/rsync --remove-source-files --prune-empty-dirs --temp-dir=$local_temp_dir --log-file=$log -rzvIPe ssh --stats $dst:$remote_dir_tv $local_dir_tv 
 
 date >> $log
 
@@ -74,7 +115,7 @@ ssh $dst ls -R torrents/movies | grep -e .mkv -e .mp4
 echo " "
 echo "Movie Sync" >> $log
 #/usr/bin/rsync --remove-source-files --prune-empty-dirs --log-file=$log -rizcvIPe ssh $dst:$remote_dir_movies $local_dir_movies 
-/usr/bin/rsync --remove-source-files --prune-empty-dirs --partial --temp-dir=$local_temp_dir --log-file=$log -rzvIPe ssh --stats $dst:$remote_dir_movies $local_dir_movies 
+/usr/bin/rsync --remove-source-files --prune-empty-dirs --temp-dir=$local_temp_dir --log-file=$log -rzvIPe ssh --stats $dst:$remote_dir_movies $local_dir_movies 
 
 date >> $log
 echo "Movie Sync Finished"
@@ -87,3 +128,5 @@ date >> $log
 echo "done" >> $log
 echo "Finished > Exiting" >> $log
 echo "" >> $log
+
+exit 0
