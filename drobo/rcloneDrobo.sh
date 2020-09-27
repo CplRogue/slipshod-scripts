4bac428a914281326db8a91fe98a1e6aeffe87c2
CplRogue <darley+github@thewares.net>
2020-09-27 12:21:35 -0400

    Preping for new Drobo

--- drobo/rcloneDrobo.sh
+++ drobo/rcloneDrobo.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
+primary_drobo=/mnt/MrU/Videos/TeslaCam
 secondary_drobo=/mnt/MrU/RcloneTest
 
 log=$HOME/Scripts/logs/log.rclone-drobo
+level=INFO
+exclude=.DS_Store
 message=$HOME/Scripts/drobo/tmp/last.msg
 size=$HOME/Scripts/drobo/tmp/last.size
+exist=$HOME/Scripts/drobo/tmp/exist
+motd=$HOME/Scripts/drobo/tmp/motd
 mrusize=$HOME/Scripts/size/tmp/last.size
 pushover_app=$HOME/Scripts/.dontsync/pushover_app_drobo
 pushover_usr=$HOME/Scripts/.dontsync/pushover_usr
 
+##Only use one
+#dryrun=--dry-run
+dryrun=
+
+
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit; fi
 
-
-msg1=`cat $log | grep Transferred | tail -2 | head -1 | cut -f1,3 -d,`
+#rclone Backups
+/usr/bin/rclone sync $primary_drobo $secondary_drobo --log-file=$log --log-level $level --exclude $exclude $dryrun
+msg1=`cat $log | grep Transferred | tail -2 | cut -f1-3 -d,`
 msg3=`cat $log | grep Elapsed | tail -1`
-msg4=`cat $log | grep Transferred | tail -2 | head -1 | cut -f4 -d,`
 /usr/bin/rclone size $secondary_drobo > $size
 msg5=`cat $size | head -1`
 msg6=`cat $size | tail -1 | cut -f1-4 -d ' '`
-msg7=`cat $mrusize | head -n 1`
 echo "::Drobo Sync::" > $message
 echo $msg1 >> $message
 echo $msg3 >> $message
-echo $msg4 >> $message
 echo $msg5 >> $message
 echo $msg6 >> $message
-echo $msg7 >> $message
+cat $message | grep "Elapsed time: 0." > $exist
+#if [[ -s $exist ]] ; then
+#  echo "$exist for Backups is zero."
+#else
 cat $message
 curl -s \
   --form-string "token=`cat $pushover_app`" \
@@ -33,5 +44,6 @@ curl -s \
   --form-string "priority=-2" \
   --form-string "message=`cat $message`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+#fi ;
 
 exit 0
