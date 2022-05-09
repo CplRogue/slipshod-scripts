43ed8efcc29e14d67b23882def34138cff06f318
CplRogue <darley+github@thewares.net>
2022-05-09 09:23:06 -0400

    Adding new drobo sync scripts

--- rsync_pull/drobo_sync.sh
+++ rsync_pull/drobo_sync.sh
@@ -2,9 +2,17 @@
 
 local_temp_dir=/mnt/Lenore/Working
 local_dir=/mnt/Lenore/Videos/
+local_dir_music=/mnt/Lenore/Music/
 
 log=$HOME/Scripts/logs/log.rsyncpull
-message=$HOME/Scripts/rsync_pull/tmp/last.msg
+
+message_anime=$HOME/Scripts/rsync_pull/tmp/anime.msg
+message_kids=$HOME/Scripts/rsync_pull/tmp/kids.msg
+message_mature=$HOME/Scripts/rsync_pull/tmp/mature.msg
+message_tv=$HOME/Scripts/rsync_pull/tmp/tv.msg
+message_movies=$HOME/Scripts/rsync_pull/tmp/movies.msg
+message_music=$HOME/Scripts/rsync_pull/tmp/music.msg
+
 pushover_app=$HOME/Scripts/.dontsync/pushover_app_drobo
 pushover_usr=$HOME/Scripts/.dontsync/pushover_usr
 
@@ -13,10 +21,13 @@ remote_dir_kids=/mnt/MrU/Videos/Kids
 remote_dir_mature=/mnt/MrU/Videos/Mature
 remote_dir_tv=/mnt/MrU/Videos/Series
 remote_dir_movies=/mnt/MrU/Videos/Movies
+remote_dir_music=/mnt/MrU/Music
 
 #settings="--stats --progress --delete-during --size-only -rzv"
 settings="--stats --progress --delete-during --max-delete=10 --size-only -rzv"
+settings2="--stats --progress --size-only -rzv"
 #dryrun=--dry-run
+dryrun2=--dry-run
 dryrun=
 
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then echo "Already running"; exit; fi
@@ -74,14 +85,19 @@ date
 echo " "
 echo "TV Sync Finished" >> $log
 date >> $log
-echo "TV Sync Finished" > $message
-tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message
+echo "TV Sync Finished" > $message_tv
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_tv
+tv_exists=$(tail -n 3 $message_tv | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$tv_exists" ] 
+	then echo "Empty TV Message"
+else 
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
   --form-string "priority=-2" \
-  --form-string "message=`cat $message`" \
+  --form-string "message=`cat $message_tv`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
 
 echo "Mature Sync"
 echo "Mature Sync" >> $log
@@ -93,14 +109,19 @@ date
 echo " "
 echo "Mature Sync Finished" >> $log
 date >> $log
-echo "Mature Sync Finished" > $message
-tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message
+echo "Mature Sync Finished" > $message_mature
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_mature
+mature_exists=$(tail -n 3 $message_mature | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$mature_exists" ] 
+	then echo "Empty Mature Message"
+else 
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
   --form-string "priority=-2" \
-  --form-string "message=`cat $message`" \
+  --form-string "message=`cat $message_mature`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
 
 echo "Kids Sync"
 echo "Kids Sync" >> $log
@@ -112,14 +133,19 @@ date
 echo " "
 echo "Kids Sync Finished" >> $log
 date >> $log
-echo "Kids Sync Finished" > $message
-tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message
+echo "Kids Sync Finished" > $message_kids
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_kids
+kids_exists=$(tail -n 3 $message_kids | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$kids_exists" ] 
+	then echo "Empty Kids Message"
+else 
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
   --form-string "priority=-2" \
-  --form-string "message=`cat $message`" \
+  --form-string "message=`cat $message_kids`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
 
 echo "Anime Sync"
 echo "Anime Sync" >> $log
@@ -131,18 +157,23 @@ date
 echo " "
 echo "Anime Sync Finished" >> $log
 date >> $log
-echo "Anime Sync Finished" > $message
-tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message
+echo "Anime Sync Finished" > $message_anime
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_anime
+anime_exists=$(tail -n 3 $message_anime | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$anime_exists" ] 
+	then echo "Empty Anime Message"
+else 
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
   --form-string "priority=-2" \
-  --form-string "message=`cat $message`" \
+  --form-string "message=`cat $message_anime`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
 
 echo "Movie Sync"
 echo "Movie Sync" >> $log
-/usr/bin/rsync --temp-dir=$local_temp_dir --log-file=$log $settings -e 'ssh -p 4432' mal@home.thewares.net:$remote_dir_movie $local_dir $dryrun
+/usr/bin/rsync --temp-dir=$local_temp_dir --log-file=$log $settings -e 'ssh -p 4432' mal@home.thewares.net:$remote_dir_movies $local_dir $dryrun
 #echo "Movie Sync Disabled"
 date >> $log
 echo "Movie Sync Finished"
@@ -150,18 +181,49 @@ date
 echo " "
 echo "Movie Sync Finished" >> $log
 date >> $log
-echo "Movie Sync Finished" > $message
-tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message
+echo "Movie Sync Finished" > $message_movies
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_movies
+movies_exists=$(tail -n 3 $message_movies | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$movies_exists" ] 
+	then echo "Empty Movies Message"
+else 
 curl -s \
   --form-string "token=`cat $pushover_app`" \
   --form-string "user=`cat $pushover_usr`" \
   --form-string "priority=-2" \
-  --form-string "message=`cat $message`" \
+  --form-string "message=`cat $message_movies`" \
   https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
+
+echo "Music Sync"
+echo "Music Sync" >> $log
+/usr/bin/rsync --temp-dir=$local_temp_dir --log-file=$log $settings2 -e 'ssh -p 4432' mal@home.thewares.net:$remote_dir_music $local_dir_music $dryrun2
+#echo "Music Sync Disabled"
+date >> $log
+echo "Music Sync Finished"
+date
+echo " "
+echo "Music Sync Finished" >> $log
+date >> $log
+echo "Music Sync Finished" > $message_music
+tail -n 20 $log | grep -B 17 Finished | grep Number | awk '{$1=$2=$3=""; print $0}' >> $message_music
+music_exists=$(tail -n 3 $message_music | sed -r 's/[a-zA-Z0:,()]//g' | awk '{if(NF>0) {print $0}}')
+if [ -z "$music_exists" ] 
+	then echo "Empty Music Message"
+else 
+curl -s \
+  --form-string "token=`cat $pushover_app`" \
+  --form-string "user=`cat $pushover_usr`" \
+  --form-string "priority=-2" \
+  --form-string "message=`cat $message_music`" \
+  https://api.pushover.net/1/messages.json > /dev/null 2>&1
+fi
+
 
 echo "done" >> $log
 echo "Finished > Exiting" >> $log
 date >> $log
 echo "" >> $log
+
 
 exit 0
