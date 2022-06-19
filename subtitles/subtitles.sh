05d91674025735c2df8e2c46c4068d79d7ff9b78
CplRogue <darley+github@thewares.net>
2022-06-19 12:41:59 -0400

    Adding Substiles

---
+++ subtitles/subtitles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+cd /mnt/MrU/Videos/Series
+#cd /mnt/MrU/Videos/Movies
+#cd /mnt/MrU/Videos/Mature
+
+#cd ~/Scripts/subtitles/test/
+
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Advertise your product or brand here/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/OpenSubtitles/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/osdb.link/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/best subtitles/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/VIP member/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/subscene/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/AllSubs/d'
+
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/addic7ed/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/facebook.com/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/live.com/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/corrected by/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/sync/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Sync/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Transcri/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Sincroniz/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Uploaded by/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Subtitles/d'
+
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Captioning sponsored/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/Captioned by/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/wgbh.org/d'
+#find . -type f -name "*.srt" -print0 | xargs -0 sed -i '/flixify/d'
